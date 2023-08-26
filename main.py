ce1d09b1d1800f3b147422f74e062f9f8edabfcc
H.Sertac Serdar <hsertacserdar@gmail.com>
2023-08-26 23:04:01 +0300

    Wheather Application Requests Python

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 import requests
 
 #
-api_key = input("Enter Your API key: ")
+api_key = "8c89d38415e7cd7cbcf6f0ee694df251"
 
 city = input("Enter city name: ")
 
@@ -10,11 +10,11 @@ url = "http://api.openweathermap.org/data/2.5/weather?q="+city+"&appid="+api_key
 
 response = requests.get(url)
 
-if response.status_code != 200:
+if response.json()["cod"] == 200:
     data = response.json()
-    temp = data["main"]["temp"]
+    temp = round(data["main"]["temp"]-273)
     desc = data["weather"][0]["description"]
-    print(f"Temperature: {temp} K")
+    print(f"Temperature: {temp} C")
     print(f"Description: {desc}")
 else:
     print("Invalid API Key")
